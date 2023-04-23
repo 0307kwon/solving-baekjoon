a49da1498614f85e7ca8427b5abfc6cd52b9513b
0307kwon <0307kwon@gmail.com>
2023-04-23 13:49:06 +0900

    update: 곱셈 문제 풀이

--- 문제집/단기간_성장/1992/index.js
+++ 문제집/단기간_성장/1992/index.js
@@ -8,80 +8,28 @@ const inputs = require("fs")
   .toString()
   .split("\n");
 
-// 재귀로
-const N = +inputs[0];
+const input = inputs[0].split(" ").map((v) => +v);
+const A = BigInt(input[0]);
+const B = input[1];
+const C = BigInt(input[2]);
 
-const picture = [...new Array(N)].map(() => [...new Array(N)]);
-
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    picture[i][j] = +inputs[i + 1][j];
-  }
-}
-
-// [y][x]
-const recursive = (range) => {
-  const { start, end } = range;
-
-  const n = end.x - start.x + 1;
-
-  if (n === 2) {
-    // 종료 조건
-    const stringSum = `${picture[start.y][start.x]}${picture[start.y][end.x]}${
-      picture[end.y][start.x]
-    }${picture[end.y][end.x]}`;
-
-    if (stringSum === "0000") {
-      return "0";
-    }
-
-    if (stringSum === "1111") {
-      return "1";
-    }
-
-    return `(${stringSum})`;
+const getRemainder = (exponent) => {
+  if (exponent === 1) {
+    return A % C;
   }
 
-  // 가로 세로 반 잘라서 4번 리커시브
-  const topLeft = recursive({
-    start,
-    end: { x: start.x + n / 2 - 1, y: start.y + n / 2 - 1 },
-  });
-  const topRight = recursive({
-    start: { x: start.x + n / 2, y: start.y },
-    end: { x: end.x, y: end.y - n / 2 },
-  });
-  const bottomLeft = recursive({
-    start: { x: start.x, y: start.y + n / 2 },
-    end: { x: end.x - n / 2, y: end.y },
-  });
-  const bottomRight = recursive({
-    start: { x: start.x + n / 2, y: start.y + n / 2 },
-    end,
-  });
+  // log n으로 연산량을 줄이기 위해 2로 나눔.
+  // k = A^(exponent / 2) % C
+  // k = BigInt
+  const k = getRemainder(Math.floor(exponent / 2));
 
-  const sumString = `${topLeft}${topRight}${bottomLeft}${bottomRight}`;
-
-  if (sumString === "0000") {
-    return "0";
+  // 짝수
+  if (exponent % 2 === 0) {
+    return (k * k) % C;
   }
 
-  if (sumString === "1111") {
-    return "1";
-  }
-
-  return `(${sumString})`;
+  // 홀수
+  return (k * k * (A % C)) % C;
 };
 
-let answer;
-
-if (N === 1) {
-  answer = inputs[1];
-} else {
-  answer = recursive({
-    start: { x: 0, y: 0 },
-    end: { x: N - 1, y: N - 1 },
-  });
-}
-
-console.log(answer);
+console.log(getRemainder(B).toString());
